6973517d13e6d49db99ca4e0b8bf90021e2cefba
zsiguli <markovichzsigmond@gmail.com>
2017-06-05 17:05:25 +0200

    feat(index): establish connection between index template and the meals in database

--- src/main/java/com/zsiguli/controller/MainController.java
+++ src/main/java/com/zsiguli/controller/MainController.java
@@ -1,6 +1,9 @@
 package com.zsiguli.controller;
 
+import com.zsiguli.model.Meal;
 import com.zsiguli.model.TypeOfMeal;
+import com.zsiguli.repository.MealRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+  @Autowired
+  MealRepository mealRepository;
+
   @GetMapping("/")
-  public String homePage() {
+  public String homePage(Model model) {
+    mealRepository.save(new Meal());
+    mealRepository.save(new Meal());
+    mealRepository.save(new Meal());
+    model.addAttribute("meals", mealRepository.findAll());
     return "index";
   }
 
