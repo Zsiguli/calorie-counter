a43c6a1d8671c8784cfd6244f4c28902bf1e6e70
zsiguli <markovichzsigmond@gmail.com>
2017-06-05 14:38:10 +0200

    chore(calorieApp): project structure

---
+++ src/main/java/com/zsiguli/CalorieCounterProjectApplication.java
@@ -0,0 +1,12 @@
+package com.zsiguli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieCounterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieCounterProjectApplication.class, args);
+	}
+}
